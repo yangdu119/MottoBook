cbbd418e820a74eec62b22141eb0883a4bfc3c96
Yang Du <ydu@rubiconproject.com>
2018-01-23 00:07:42 -0800

    add missing files

---
+++ src/AppSync.js
@@ -0,0 +1,6 @@
+﻿export default {
+    "graphqlEndpoint": "https://t6d7pyo2eff27non5xftccsjle.appsync-api.us-west-2.amazonaws.com/graphql",
+    "region": "us-west-2",
+    "authenticationType": "API_KEY",
+    "apiKey": "da1-_6908xTBQ3qAHDTHSmsogQ"
+}
