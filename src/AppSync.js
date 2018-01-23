4f3e6ece01f0e5e232bded8b5fef52ce0dda8f24
Yang Du <ydu@rubiconproject.com>
2018-01-23 00:15:28 -0800

    update key

--- src/AppSync.js
+++ src/AppSync.js
@@ -2,5 +2,5 @@
     "graphqlEndpoint": "https://t6d7pyo2eff27non5xftccsjle.appsync-api.us-west-2.amazonaws.com/graphql",
     "region": "us-west-2",
     "authenticationType": "API_KEY",
-    "apiKey": "da1-_6908xTBQ3qAHDTHSmsogQ"
+    "apiKey": "da1-59z2NGhRRX2qO5dzfUWR3Q"
 }
