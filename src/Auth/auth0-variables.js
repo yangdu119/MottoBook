e4685ea8cc3483d3e16d97d6b44c0e96892c6fe5
Yang Du <ydu@rubiconproject.com>
2018-02-02 01:21:38 -0800

    change to production callback

--- src/Auth/auth0-variables.js
+++ src/Auth/auth0-variables.js
@@ -1,5 +1,5 @@
 export const AUTH_CONFIG = {
   domain: 'mottobook.auth0.com',
   clientId: 'b0hq8P8iCtqUGL7AdqTj8vcjhhbk3pZV',
-  callbackUrl: 'http://localhost:3000/callback'
+  callbackUrl: 'http://www.mottobook.com/callback'
 }
