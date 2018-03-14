c07dc43b60b5bc6f7604db0f539dde66f8c678be
yangdu119 <du_bill@hotmail.com>
2018-03-14 11:00:52 -0700

    Update Auth.js

--- src/Auth/Auth.js
+++ src/Auth/Auth.js
@@ -1,6 +1,8 @@
 import auth0 from 'auth0-js';
 import { AUTH_CONFIG } from './auth0-variables';
 import history from '../history';
+import {createApolloFetch} from 'apollo-fetch';
+import graphqlEndPoint from "../GraphQLConfig"
 
 export default class Auth {
   auth0 = new auth0.WebAuth({
@@ -21,10 +23,59 @@ export default class Auth {
     this.isAuthenticated = this.isAuthenticated.bind(this);
     this.getAccessToken = this.getAccessToken.bind(this);
     this.getProfile = this.getProfile.bind(this);
+    this.submitProfile = this.submitProfile.bind(this);
+    this.getUserProfile = this.getUserProfile.bind(this);
+    this.getUserId = this.getUserId.bind(this);
+    this.getUserIDHelper = this.getUserIDHelper.bind(this);
   }
+
+  getUserProfile = (that) => new Promise(function(resolve, reject){
+      if (!that.userProfile) {
+          that.getProfile((err, profile) => {
+              resolve(profile)
+          });
+      } else {
+          resolve(that.userProfile)
+      }
+  })
+
+  getUserId = (sub) => new Promise(function(resolve, reject){
+      const uri = graphqlEndPoint.prisma
+      const apolloFetch = createApolloFetch({uri})
+      const query = `
+          query{
+            user(where:{sub: "${sub}"}){
+              id
+            }
+          }
+      `
+
+      apolloFetch({query})
+          .then(result => {
+              const {data} = result;
+              if (data){
+                  resolve(data.user.id)
+              }
+          })
+  })
+
+    async getUserIDHelper(){
+        const {sub} = await this.getUserProfile(this);
+        const userId = await this.getUserId(sub)
+        this.userProfile = {
+            ...this.userProfile,
+            prisma_userId: userId,
+        }
+        const userProfile = localStorage.getItem('userProfile');
+        localStorage.setItem('userProfile', {
+            ...userProfile,
+            prisma_userId: userId,
+        });
+    }
 
   login() {
     this.auth0.authorize();
+    this.getProfile();
   }
 
   handleAuthentication() {
@@ -60,11 +111,50 @@ export default class Auth {
     return accessToken;
   }
 
+  submitProfile() {
+      console.log('userProfile', this.userProfile);
+      const {email, family_name, given_name, locale, name, nickname, picture, sub} = this.userProfile;
+      const uri = graphqlEndPoint.prisma;
+      const apolloFetch = createApolloFetch({uri})
+
+      const mutationQuery = `
+            mutation createUser{
+                createUser(data:{email:"${email}"
+                givenname:"${given_name}"
+                familyname:"${family_name}"
+                nickname:"${nickname}"
+                name:"${name}"
+                picture:"${picture}"
+                locale:"${locale}"
+                sub:"${sub}"})
+                {
+                    sub
+                }
+            }
+            
+      `
+
+      apolloFetch({query:mutationQuery})
+          .then(result => {
+              const {errors} = result;
+              if (errors){
+                  if (errors[0].code ===3010){
+                      console.log("user already exists")
+                  }
+              }
+
+              //console.log('result', result)
+          })
+
+  }
+
   getProfile(cb) {
     let accessToken = this.getAccessToken();
     this.auth0.client.userInfo(accessToken, (err, profile) => {
       if (profile) {
         this.userProfile = profile;
+        localStorage.setItem('userProfile', profile);
+        this.submitProfile()
       }
       cb(err, profile);
     });
@@ -75,6 +165,7 @@ export default class Auth {
     localStorage.removeItem('access_token');
     localStorage.removeItem('id_token');
     localStorage.removeItem('expires_at');
+    localStorage.removeItem('userProfile');
     this.userProfile = null;
     // navigate to the home route
     history.replace('/home');
